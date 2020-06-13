8bf28102ae750b3ca65ff2093bb7ae0a7589bb22
Daniel Corcoran <danielc.92@outlook.com>
2020-06-13 12:39:28 +1000

    Structs

--- src/main.rs
+++ src/main.rs
@@ -27,22 +27,64 @@
 // }
 
 // Arrays and tuples
+// fn main() {
+//     let num_array = [2, 3, 4];
+
+//     let num_tuple = (3, 5, 4);
+
+//     // Fixed size array declaration
+//     let fixed_array: [i32; 3] = [1, 2, 1];
+
+//     // Return array length
+//     println!("{}", num_array.len());
+
+//     // Return value at index
+//     println!("{}", num_array[1]);
+
+//     // Check if number is in array
+//     let num = 3;
+//     let result = num_array.contains(&num);
+//     println!("{}", result);
+// }
+
+// Custom types
 fn main() {
-    let num_array = [2, 3, 4];
+    struct Task {
+        is_complete: bool,
+        title: String,
+        timestamp: u64,
+    }
 
-    let num_tuple = (3, 5, 4);
+    let mut daniels_task: Task = Task {
+        is_complete: false,
+        title: "First task of the day".to_string(),
+        timestamp: 156465465456,
+    };
 
-    // Fixed size array declaration
-    let fixed_array: [i32; 3] = [1, 2, 1];
+    println!(
+        "Daniels task {} {} {}",
+        daniels_task.is_complete, daniels_task.title, daniels_task.timestamp
+    );
 
-    // Return array length
-    println!("{}", num_array.len());
+    daniels_task.title = "I changed my mind".to_string();
 
-    // Return value at index
-    println!("{}", num_array[1]);
+    println!(
+        "Daniels task {} {} {}",
+        daniels_task.is_complete, daniels_task.title, daniels_task.timestamp
+    );
 
-    // Check if number is in array
-    let num = 3;
-    let result = num_array.contains(&num);
-    println!("{}", result);
+    struct Coordinate {
+        longitude: f64,
+        latitude: f64,
+    }
+
+    let hometown: Coordinate = Coordinate {
+        longitude: 144.5456,
+        latitude: -37.654,
+    };
+
+    println!(
+        "Hometown coordinates {} {}",
+        hometown.longitude, hometown.latitude
+    );
 }
