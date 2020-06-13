910f5284e8c0b9325d62f57711a0db1ec0328960
Daniel Corcoran <danielc.92@outlook.com>
2020-06-13 15:08:49 +1000

    Test out two functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 mod array_stuff;
 mod bool_stuff;
+mod function_stuff;
 mod struct_stuff;
 mod variable_stuff;
 
 fn main() {
-    variable_stuff::try_it_out();
+    println!(
+        "{} {} {} {} {}",
+        function_stuff::area_of_square(2),
+        function_stuff::area_of_square(3),
+        function_stuff::area_of_square(4),
+        function_stuff::area_of_square(0),
+        function_stuff::area_of_square(-3)
+    );
+
+    let nums = [3, 54, 3, 52, 34, 423, 4];
+    function_stuff::sum_array_nums(&nums);
 }
