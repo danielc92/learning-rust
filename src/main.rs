e309a0f8ff2983ec09a11e4bdf83f05828ef6caf
Daniel Corcoran <danielc.92@outlook.com>
2020-06-14 19:52:38 +1000

    Testing out vecs

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,9 @@ mod array_stuff;
 mod bool_stuff;
 mod function_stuff;
 mod struct_stuff;
+mod token_stuff;
 mod variable_stuff;
 
 fn main() {
-    let name = "Daniel Corcoran".to_owned();
-    let result = function_stuff::if_elsing(&name);
-    print!("{}", result);
+    array_stuff::try_it_out();
 }
