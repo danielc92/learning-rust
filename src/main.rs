527ad9aadc36b1412ab4d2507623b7d80dbfad3a
Daniel Corcoran <danielc.92@outlook.com>
2020-06-17 14:05:14 +1000

    Test out api call to json placeholder

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,24 @@ mod struct_stuff;
 mod token_stuff;
 mod variable_stuff;
 
-fn main() {
-    println!(
-        "{:?}",
-        algorithm_stuff::binary_search(&[1, 2, 3, 4, 5, 6, 7, 8, 10, 13], 7)
-    );
+use reqwest::Error;
+use serde::Deserialize;
 
-    println!(
-        "{:?}",
-        algorithm_stuff::linear_search(&[1, 2, 3, 4, 5, 6, 7, 8, 10, 13], &10)
-    );
+#[derive(Deserialize, Debug)]
+struct User {
+    userId: i32,
+    id: i32,
+    title: String,
+    completed: bool,
 }
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let response = reqwest::get("https://jsonplaceholder.typicode.com/todo5s").await?;
+    println!("{}", response.status());
+    let users: Vec<User> = response.json().await?;
+    println!("{:?}", users);
+    Ok(())
+}
+
+// https://jsonplaceholder.typicode.com/todos
