644cd530835165e6d4cfad59bc1bb9a98f95b8c2
Daniel Corcoran <danielc.92@outlook.com>
2020-06-17 18:55:23 +1000

    Error handling example with file read

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -5,3 +5,17 @@ pub fn string_to_i32(user_input: &String) -> String {
         Err(i32) => "Failure".to_owned(),
     }
 }
+
+pub fn file_read_to_succeed() -> String {
+    match std::fs::File::open("C:\\Users\\dc\\Desktop\\png\\add.png") {
+        Ok(file) => "Found the file".to_string(),
+        Err(err) => "Error occured".to_string(),
+    }
+}
+
+pub fn file_read_to_fail() -> String {
+    match std::fs::File::open("8649849849") {
+        Ok(file) => "Found the file".to_string(),
+        Err(err) => "Error occured".to_string(),
+    }
+}
